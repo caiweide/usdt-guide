57f3e2995f1fce5e00043ea5011fc22041c908f3
caiweide🔥 <caiweide99@gmail.com>
2023-10-20 16:52:13 +0700

    build release 202310201652

--- release.sh
+++ release.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# Remove old usdt-guide folder
+rm -rf usdt-guide
+
 # Run install
 npm install
 
 # Run build
 npm run build
 
+# Rename dist to usdt-guide
+mv dist usdt-guide
+
 # Remove old zip file
 rm -rf usdt-guide-*.zip
 
-# Zip all files in dist folder excluding main.js, zip file name is usdt-guide-<date-time>.zip
+# Zip all files in usdt-guide folder excluding main.js, zip file name is usdt-guide-<date-time>.zip
 
-zip -r usdt-guide-$(date +%Y%m%d%H%M).zip dist -x dist/main.js
+zip -r usdt-guide-$(date +%Y%m%d%H%M).zip usdt-guide -x usdt-guide/main.js
 
 # Git commit and push, commit message is build release <date-time>
 
